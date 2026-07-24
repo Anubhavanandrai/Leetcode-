6aace985fbe253fa589f3ecc297c82a0c6ccd756
Anubhavanandrai <115334131+Anubhavanandrai@users.noreply.github.com>
2026-07-24 23:29:15 +0530

    0026-REMOVE-DUPLICATES-FROM-SORTED-ARRAY Stats: Time: 1 ms (78.12%), Space: 46.7 MB (43.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -5,6 +5,7 @@ class Solution {
       int len= nums.length;
       while(j<len)
       {
+        //duplicate element then increase pointer
         if(nums[i]==nums[j])
           {
              j++;
@@ -13,17 +14,20 @@ class Solution {
           {
             if(j==i+1)
              {
+                //consecutive elements nothing just movee forward
                 i++;
                 j++;
              }
             else
              {
+                //we wont do swap here because swapping will put visited element also for comparison again
                 i++;
                 nums[i] = nums[j];
                 j++;
              }
            }     
       }
+//because we are starting from i=0;      
 return i+1;
     }
 }
